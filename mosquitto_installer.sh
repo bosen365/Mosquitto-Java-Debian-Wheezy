7e9867f93521a6fbe1d4fbd5b738c791696cba36
mauamy <ymdev@xmau.de>
2017-05-23 14:31:13 +0200

    Update mosquitto_installer.sh

--- mosquitto_installer.sh
+++ mosquitto_installer.sh
@@ -10,18 +10,11 @@ cd
 sudo apt-get update
 sudo apt-get install mosquitto mosquitto-clients mosquitto-dev
 
-mkdir MQTT_Project
-cd MQTT_Project
-
-#git clone https://github.com/eclipse/paho.mqtt.java.git
- 
-#sudo apt-get install maven
-#mvn package -DskipTests
-
-#cp paho.mqtt.java/org.eclipse.paho.client.mqttv3/target/org.eclipse.paho.client.mqttv3-1.1.1.jar .
-
-# https://mosquitto.org/download/
-# https://mosquitto.org/2013/01/mosquitto-debian-repository/
-# http://tgrall.github.io/blog/2017/01/02/getting-started-with-mqtt/
-# https://github.com/eclipse/paho.mqtt.java
-# https://github.com/tgrall/mqtt-sample-java
+### UNCOMMENT THESE LINES FOR BUILDING FROM SOURCE ###
+# mkdir MQTT_Project
+# cd MQTT_Project
+# git clone https://github.com/eclipse/paho.mqtt.java.git 
+# sudo apt-get install maven
+# mvn package -DskipTests
+# cp paho.mqtt.java/org.eclipse.paho.client.mqttv3/target/org.eclipse.paho.client.mqttv3-1.1.1.jar .
+### UNCOMMENT THESE LINES FOR BUILDING FROM SOURCE ###
